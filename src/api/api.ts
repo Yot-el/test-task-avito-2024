f75f7800c76d324dee46d0a5a6d93187bd32d410
Yot-el <yvsuteots@gmail.com>
2024-04-09 20:37:52 +0200

    feat: api function

---
+++ src/api/api.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-unreachable */
+import { AbortError, ServerResponseError } from "models/classes";
+
+const BASE_URL = "https://api.kinopoisk.dev";
+
+const headers: HeadersInit = {
+	accept: "application/json",
+	"X-API-KEY": process.env.TOKEN ?? ""
+};
+
+const options: RequestInit = {
+	method: "GET",
+	headers: headers
+};
+
+export const fetchData = async <T>(url: string, signal?: AbortSignal): Promise<T> => {
+	try {
+		const response = await fetch(`${BASE_URL}${url}`, { ...options, signal });
+
+		if (!response.ok) {
+			throw new ServerResponseError(response.status);
+		}
+
+		const data = await response?.json();
+		return data;
+	} catch (e: unknown) {
+		if (e instanceof Error) {
+			if (e.name === "AbortError") {
+				throw new AbortError();
+			}
+		}
+
+		throw e;
+	}
+};
