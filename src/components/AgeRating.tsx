4956590e774a97ebe5c944c290973af8dbed467b
Yot-el <yvsuteots@gmail.com>
2024-04-11 16:24:54 +0200

    feat: update AgeRating component

--- src/components/AgeRating.tsx
+++ src/components/AgeRating.tsx
@@ -1,12 +1,15 @@
-import { NumberFormatter } from "@mantine/core";
+import { NumberFormatter, Text } from "@mantine/core";
 
 interface AgeRatingProps {
   value: number;
+	size?: string | "xs";
 }
 
-const AgeRating = ({ value }: AgeRatingProps) => {
+const AgeRating = ({ value, size }: AgeRatingProps) => {
 	return (
-		<NumberFormatter suffix="+" value={value} />
+		<Text size={size} inline>
+			<NumberFormatter suffix="+" value={value}/>
+		</Text>
 	);
 };
 
